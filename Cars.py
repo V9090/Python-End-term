0589d03fc25bd64ce245a71e9d794b1616401cad
V9090 <92574260+V9090@users.noreply.github.com>
2021-11-10 03:00:49 +0530

    Update Cars.py

--- Cars.py
+++ Cars.py
@@ -51,7 +51,7 @@ st.markdown('Price variation of based on location')
 plt.show()
 fig1 = sns.boxplot(x=df.Location,y=df.Price,data=df,width=.6)
 plt.ylim(0,60) 
-st.pyplot(fig)
+st.pyplot(fig1)
 
 
 location_list=df.Location.unique().tolist()
