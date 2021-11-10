287113ae40686502ff3c4e8cbbc8b5b7518785d6
V9090 <92574260+V9090@users.noreply.github.com>
2021-11-10 15:34:02 +0530

    Update Cars.py

--- Cars.py
+++ Cars.py
@@ -57,7 +57,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 fig = plt.figure(figsize=(12, 6))
 if sd == "Dist Plot":
         plt.xlim(0,200000) 
-        sns.displot(data=df,x=df.Km, color= 'red')
+        sns.barplot(data=df,x=df.Km, color= 'red')
         plt.show()
     
 elif sd == "Box Plot":
