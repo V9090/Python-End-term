73616876c59b85a74a1ac9c71fde7ed60befbd5c
V9090 <92574260+V9090@users.noreply.github.com>
2021-11-10 15:40:23 +0530

    Update Cars.py

--- Cars.py
+++ Cars.py
@@ -57,19 +57,21 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 fig = plt.figure(figsize=(12, 6))
 if sd == "Dist Plot":
         plt.xlim(0,200000) 
-        fig=sns.displot(data=df,x=df.Km, color= 'red')
+        sns.displot(data=df,x=df.Km, color= 'red')
         plt.show()
+        st.pyplot()
     
 elif sd == "Box Plot":
         plt.ylim(0,60)
-        fig=sns.boxplot(x=df.Location,y=df.Price,data=df,width=.6)
+        sns.boxplot(x=df.Location,y=df.Price,data=df,width=.6)
         plt.show()
+        st.pyplot()
 
 # st.markdown('Price variation of based on location')
 # plt.show()
 # fig1 = sns.boxplot(x=df.Location,y=df.Price,data=df,width=.6)
 # plt.ylim(0,60) 
-st.pyplot(fig)
+
 
 
 location_list=df.Location.unique().tolist()
