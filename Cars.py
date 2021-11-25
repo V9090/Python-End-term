b6c556d861e612eea4df554786fced09b058e617
Vishnu S Kumar <92574260+V9090@users.noreply.github.com>
2021-11-25 10:36:11 +0530

    Update Cars.py

--- Cars.py
+++ Cars.py
@@ -118,8 +118,8 @@ try:
 
 
 
-fig1 = sns.boxplot(x=df.Location,y=df.Price,data=df,width=.6)
-plt.ylim(0,60) 
+    fig1 = sns.boxplot(x=df.Location,y=df.Price,data=df,width=.6)
+    plt.ylim(0,60) 
 
 
 
